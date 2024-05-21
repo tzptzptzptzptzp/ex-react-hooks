8e85e7f442cb15626e3812a8a39511b4664a26ee
tzptzptzptzptzp <git@tzp.pink>
2024-05-22 01:44:14 +0900

    ⚗️ useCallbackの機能検証

--- src/pages/useCallback.tsx
+++ src/pages/useCallback.tsx
@@ -1,11 +1,27 @@
+import { useCallback, useState } from "react";
+
 export default function useCallbackPage() {
+  const [count, setCount] = useState(0);
+
+  // この関数は、コンポーネントが再レンダリングするたびに再作成される。
+  const handleClick = () => {
+    console.log(`Button clicked ${count} times`);
+  };
+
+  // この関数は、count が変更されたときだけ再作成される。
+  const handleClickUseCallback = useCallback(() => {
+    console.log(`Button clicked ${count} times`);
+  }, [count]);
+
   return (
     <div>
       <h1>useCallback Example</h1>
-      <input type="text" />
-      <button>Click Me</button>
-      <button>Increment Count</button>
-      <p>Button clicked 0 times</p>
+      <div className="flex gap-2">
+        <button onClick={handleClick}>Click Me (normal)</button>
+        <button onClick={handleClickUseCallback}>Click Me (useCallback)</button>
+        <button onClick={() => setCount(count + 1)}>Increment Count</button>
+      </div>
+      <p>Button clicked {count} times</p>
     </div>
   );
 }
