35d708ce63f93ebdd90005355082e33ff206338b
tzptzptzptzptzp <git@tzp.pink>
2024-05-22 02:04:03 +0900

    ✨ Providerを設定

--- src/pages/useContext.tsx
+++ src/pages/useContext.tsx
@@ -1,7 +1,30 @@
+import { ReactNode, createContext, useState } from "react";
+
+export const ThemeContext = createContext({
+  theme: "light",
+  toggleTheme: () => {},
+});
+
+export const ThemeProvider = ({ children }: { children: ReactNode }) => {
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
 export default function useContextPage() {
   return (
-    <div>
-      <h1>useContext Example</h1>
-    </div>
+    <ThemeProvider>
+      <div>
+        <h1>useContext Example</h1>
+      </div>
+    </ThemeProvider>
   );
 }
